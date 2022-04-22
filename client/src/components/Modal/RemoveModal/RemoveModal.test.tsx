6343c8452dd682299f80ddf37c2b77a324b46438
Alp Tarla <alptarla@gmal.com>
2022-04-22 21:06:24 +0300

    Create remove modal

---
+++ client/src/components/Modal/RemoveModal/RemoveModal.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { mockJobs } from '../../../testEnvoirments'
+import RemoveModal from './RemoveModal'
+
+const mockJob = mockJobs[0]
+
+test('should renders correctly', () => {
+  render(
+    <RemoveModal
+      isShow={true}
+      onClose={jest.fn()}
+      onRemove={jest.fn()}
+      job={mockJob}
+    />
+  )
+
+  expect(
+    screen.getByText(/Are you sure you want to delete it?/i)
+  ).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /approve/i })).toBeInTheDocument()
+})
--- client/src/components/Modal/RemoveModal/RemoveModal.tsx
+++ client/src/components/Modal/RemoveModal/RemoveModal.tsx
@@ -1,4 +1,44 @@
-function RemoveModal() {
-  return <div>RemoveModal</div>
+import { ReactComponent as AlertIcon } from '../../../assets/icons/ic-alert.svg'
+import { IJob } from '../../../types'
+import { Button } from '../../Elements'
+import Modal from '../Modal'
+import classes from './RemoveModal.module.scss'
+
+interface IProps {
+  isShow?: boolean
+  onClose: VoidFunction
+  onRemove: (job: IJob) => void
+  job: IJob | null
 }
+
+function RemoveModal({ isShow = true, onClose, onRemove, job }: IProps) {
+  const handleRemove = () => {
+    if (job) onRemove(job)
+  }
+
+  return (
+    <Modal
+      onClose={onClose}
+      isShow={isShow}
+      title='Are you sure you want to delete it?'
+    >
+      <AlertIcon className={classes.alertIcon} />
+      <div className={classes.action}>
+        <Button
+          variant='secondary'
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant='danger'
+          onClick={handleRemove}
+        >
+          Approve
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
 export default RemoveModal
